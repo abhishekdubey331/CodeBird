8080721eb575c31bd73b155c054cd6c2ebcfbd94
Abhishek Dubey <abhishekdubey@tnine.io>
2017-04-29 11:34:49 +0530

    first commit

---
+++ app/src/main/java/com/nasaspaceapps/codebird/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.nasaspaceapps.codebird.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nasaspaceapps.codebird.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/nasaspaceapps/codebird/activities/Splash.java
@@ -0,0 +1,15 @@
+package com.nasaspaceapps.codebird.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nasaspaceapps.codebird.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
